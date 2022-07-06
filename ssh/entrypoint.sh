cc6258a824705d62e4386663d74715f530bb4b0d
zhangzeli <zhangzeli@inmyshow.com>
2022-07-06 19:12:46 +0800

    ssh

---
+++ ssh/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# run ttyd service
+nohup ttyd -p 8686 -c root:origin123456 bash > /dev/null 2>&1 &
+
+set -e
+
+# first arg is `-f` or `--some-option`
+if [ "${1#-}" != "$1" ]; then
+    set -- /usr/sbin/sshd "$@"
+fi
+
+exec "$@"
