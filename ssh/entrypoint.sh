cb2eb07b3c766c022a191e874d5cea1bf00397ec
decezz <decezz@qq.com>
2023-05-14 11:59:44 +0800

    ssh hotfix

--- ssh/entrypoint.sh
+++ ssh/entrypoint.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-PS1='\e[1m\e[31m[\h] \e[32m(docker) \e[34m\u@$(hostname -i)\e[35m \w\e[0m\n$ '
-echo "PS1=$PS1"
-export $PS1
+# shell style
+echo "export PS1='\e[1m\e[31m[\h] \e[32m(docker) \e[34m\u@$(hostname -i)\e[35m \w\e[0m\n$ '" >> .bashrc
 
 set -e
 
-# first arg is `-f` or `--some-option`
+# first arg is `/usr/sbin/sshd`
 if [ "$1" = "/usr/sbin/sshd" ]; then
     # run ttyd service
     nohup ttyd -p 8686 -c root:origin123456 bash > /dev/null 2>&1 &
+    # change password
+    echo "root:origin123456" | chpasswd
 fi
 
 exec "$@"
