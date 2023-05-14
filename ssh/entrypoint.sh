15b563b03f724cf00b7b72b51e95ded5c53a1f45
decezz <decezz@qq.com>
2023-05-14 10:59:58 +0800

    ssh hotfix

--- ssh/entrypoint.sh
+++ ssh/entrypoint.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# run ttyd service
-nohup ttyd -p 8686 -c root:origin123456 bash > /dev/null 2>&1 &
 
 set -e
 
 # first arg is `-f` or `--some-option`
 if [ "$1" = "sshd" ]; then
-    set -- /usr/sbin/sshd "$@"
+    # run ttyd service
+    nohup ttyd -p 8686 -c root:origin123456 bash > /dev/null 2>&1 &
 fi
 
 exec "$@"
