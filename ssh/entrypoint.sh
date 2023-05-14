fd261be1cfbdc8ebedaea3974230ac445ef0a694
decezz <decezz@qq.com>
2023-05-14 10:43:45 +0800

    ssh

--- ssh/entrypoint.sh
+++ ssh/entrypoint.sh
@@ -6,7 +6,7 @@ nohup ttyd -p 8686 -c root:origin123456 bash > /dev/null 2>&1 &
 set -e
 
 # first arg is `-f` or `--some-option`
-if [ "${1#-}" != "$1" ]; then
+if [ "$1" = "sshd" ]; then
     set -- /usr/sbin/sshd "$@"
 fi
 
